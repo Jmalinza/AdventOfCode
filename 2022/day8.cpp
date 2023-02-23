f15904160269de41770e3b9e35ce615eb741dfdc
Jmalinza <jonathanmalinza@gmail.com>
2023-02-23 22:46:56 +0900

    Calculate visibile trees

--- 2022/day8.cpp
+++ 2022/day8.cpp
@@ -1,16 +1,12 @@
 #include "day8.h"
 
-#include <iostream>
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
 
 int main()
 {
     auto forest = aoc::twenty22::build_forest("../../2022/inputs/day8.txt");
-    // auto fs = aoc::twenty22::build_directory("../../2022/inputs/day8.txt");
-    // auto result = aoc::twenty22::sum_folder_sizes_under_size(fs, 100000);
-    // std::cout << "Sum of directory sizes under 100,000: " << result << "\n";
-
-    // result = aoc::twenty22::smallest_file_to_delete(fs);
-    // std::cout << "Smallest file to delete: " << result << "\n";
-    std::cout << "Hello world \n" ;
+    auto result = aoc::twenty22::count_visible_trees(forest);
+    fmt::print("Number of visible trees: {}\n", result);
     return 0;
 }
--- 2022/day8.h
+++ 2022/day8.h
@@ -1,28 +1,75 @@
 #pragma once
 
+#define FMT_HEADER_ONLY
+#include <fmt/format.h>
+#include <fmt/ranges.h>
 #include <range/v3/all.hpp>
 
 #include "utility.h"
+
+#include <tuple>
 
 using namespace aoc::utility;
 
 namespace aoc::twenty22
 {
-    // row major
-    using Forest = std::vector<std::vector<int>>;
+    // rows, cols, storage
+    using Forest = std::tuple<int, int, std::vector<int>>;
 
     Forest build_forest(std::string path)
     {
         std::vector<std::string> file(readAllLinesFrom(path));
 
-        Forest forest;
+        int rows = file.size();
+        int cols = file.at(0).size();
+        std::vector<int> data;
+        data.reserve(rows*cols);
         for(auto& line: file)
         {
             auto trees = chunks(line, 1);
-            auto heights = trees | ranges::views::transform( [](std::string t) { return std::stoi(t);} ) | ranges::to<std::vector>();
-            forest.emplace_back(heights);
+            auto heights = trees 
+                            | ranges::views::transform( [](std::string t) { return std::stoi(t);} ) 
+                            | ranges::to<std::vector>();
+            data.insert(data.end(), heights.begin(), heights.end());
         }
 
-        return forest;
+        return {rows, cols, std::move(data)};
+    }
+
+    bool is_visible(const std::vector<int>& trees, int index)
+    {
+        auto test_tree  = trees.at(index);
+        auto lower_side = std::vector<int>(trees.begin(), trees.begin() + index);
+        auto upper_side = std::vector<int>(trees.begin() + index + 1, trees.end());
+
+        auto shorter_than_test_tree = [&test_tree](int tree) { return tree < test_tree;};
+        bool is_visible_lower = ranges::all_of(lower_side, shorter_than_test_tree);
+        bool is_visible_upper = ranges::all_of(upper_side, shorter_than_test_tree);
+        
+        return is_visible_lower | is_visible_upper;
+    }
+
+    int count_visible_trees(const Forest& forest)
+    {
+        int height = std::get<0>(forest);
+        int width  = std::get<1>(forest);
+        auto& data = std::get<2>(forest);
+
+        int result = 0;
+        for(int r = 0; r < height; r++)
+        {
+            auto current_row = ranges::views::slice(data, r * height, r * height + width) | ranges::to<std::vector>();
+            for(int c = 0; c < width; c++)
+            {
+                auto current_col = data | ranges::views::drop(c) | ranges::views::stride(width) 
+                                        | ranges::to<std::vector>();
+                bool is_visible_horizontal = is_visible(current_row, c);
+                bool is_visible_vertical   = is_visible(current_col, r);
+
+                result += (is_visible_horizontal | is_visible_vertical) ? 1:0;
+            }
+        }
+
+        return result;
     }
 }
